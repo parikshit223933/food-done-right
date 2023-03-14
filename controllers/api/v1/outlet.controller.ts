0dce8c3b8cbe25a2fdb4f6ad3152586ace3a45f2
Parikshit Singh <pk223933@gmail.com>
2023-03-15 02:47:11 +0530

    controller logic

--- controllers/api/v1/outlet.controller.ts
+++ controllers/api/v1/outlet.controller.ts
@@ -1,6 +1,83 @@
-import { NextFunction, Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express';
+import { readFileSync } from 'fs';
+import * as turf from '@turf/turf';
+import path from 'path';
+import { Feature, Polygon } from '@turf/turf';
 
-export const fetchOutletIdentifier = (request: Request, response: Response, next: NextFunction) => {
-    response.json({ message: 'POST new tea' })
-    next()
-}
+export const fetchOutletIdentifier = (
+  request: Request,
+  response: Response,
+  _next: NextFunction
+) => {
+  const polygonsFeatureCollection = JSON.parse(
+    readFileSync(path.join(__dirname, '../../../../points.geo.json'), 'utf-8')
+  ); // feature collection of points
+  const polygonsCollection = turf.polygons(
+    polygonsFeatureCollection['features']
+      .filter((feature: Feature) => feature['geometry']['type'] == 'Polygon')
+      .map(
+        (feature: { geometry: { coordinates: number[] } }) =>
+          feature['geometry']['coordinates']
+      )
+  );
+  const polygonPointsCollection = turf.points(
+    polygonsFeatureCollection['features']
+      .filter((feature: Feature) => feature['geometry']['type'] == 'Point')
+      .map(
+        (feature: { geometry: { coordinates: number[] } }) =>
+          feature['geometry']['coordinates']
+      )
+  );
+  const polygonsArray = polygonsCollection.features.map((feature) =>
+    turf.polygon(feature.geometry.coordinates)
+  );
+  const pointsArray = polygonPointsCollection.features.map((feature) =>
+    turf.point(feature.geometry.coordinates)
+  );
+
+  if (!request.query.x || !request.query.y) {
+    return response.json({
+      success: false,
+      coveredByPolygon: null,
+      coveringPolygon: null,
+      deliverablePointInPolygon: null,
+      error: 'Input coordinates were not provided!',
+    });
+  }
+
+  const inputXCoordinate = parseInt(request.query.x.toString(), 10);
+  const inputYCoordinate = parseInt(request.query.y.toString(), 10);
+
+  const coveringPolygons = polygonsArray.filter((polygon) =>
+    turf.booleanPointInPolygon([inputXCoordinate, inputYCoordinate], polygon)
+  );
+  const coveredByPolygon = coveringPolygons.length !== 0;
+
+  if (coveredByPolygon) {
+    const deliverablePointsInPolygon = pointsArray.filter((point) =>
+      turf.booleanPointInPolygon(point, coveringPolygons[0])
+    );
+    return response.json({
+      coverage: {
+        success: true,
+        coveredByPolygon: coveredByPolygon,
+        coveringPolygon: coveringPolygons[0],
+        deliverablePointInPolygon:
+          deliverablePointsInPolygon.length === 0
+            ? null
+            : deliverablePointsInPolygon[0],
+        error: 'Input coordinates were not provided!',
+      },
+    });
+  } else {
+    return response.json({
+      coverage: {
+        success: true,
+        coveredByPolygon: coveredByPolygon,
+        coveringPolygon: null,
+        deliverablePointInPolygon: null,
+        error: null,
+      },
+    });
+  }
+};
