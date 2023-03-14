6d6d61278c74d85d0b0fe75089fd193adcf90500
Parikshit Singh <pk223933@gmail.com>
2023-03-14 23:01:36 +0530

    Docker next steps

--- controllers/api/v1/outlet.controller.ts
+++ controllers/api/v1/outlet.controller.ts
@@ -2,4 +2,5 @@ import { NextFunction, Request, Response } from 'express'
 
 export const fetchOutletIdentifier = (request: Request, response: Response, next: NextFunction) => {
     response.json({ message: 'POST new tea' })
+    next()
 }
--- dockerfile
+++ dockerfile
@@ -2,7 +2,5 @@ FROM node:16
 
 WORKDIR /food-done-right
 
-
-COPY package*.json ./
-RUN npm ci --only=production
 COPY . .
+RUN npm install
