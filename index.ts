8cc29f38af936f92b5752f165523bc5686dc89c9
Parikshit Singh <pk223933@gmail.com>
2023-03-15 05:37:14 +0530

    Controller :: Fixes

--- index.ts
+++ index.ts
@@ -1,19 +1,26 @@
-import express, { Express, Request, Response } from 'express'
-import dotenv from 'dotenv'
-import router from './routes/api/v1'
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import router from './routes/api/v1';
+import cors from 'cors';
 
-dotenv.config()
+dotenv.config();
 
-const app: Express = express()
-const port: string = process.env.PORT || '8000'
+const app: Express = express();
+const port: string = process.env.PORT || '8000';
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Server is running')
-})
+  res.send('Server is running');
+});
 
-app.use(express.json())
-app.use('/', router)
+app.use(
+  cors({
+    origin: '*',
+  })
+);
+
+app.use(express.json());
+app.use('/', router);
 
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
-})
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
